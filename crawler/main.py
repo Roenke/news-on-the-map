f46f2dfc1d68e8db25813047627938ae6e086b68
Fyodor Bocharov <bocharovfedor@gmail.com>
2016-10-17 17:04:26 +0300

    Fixes + vk support

--- crawler/main.py
+++ crawler/main.py
@@ -2,7 +2,21 @@ import argparse
 import json
 import os
 import time
-import rss
+from datetime import datetime
+import cPickle as pickle
+import psycopg2
+
+import rss_feed
+import vk_feed
+
+
+MAX_FEED_COUNT = 1024
+LOADED_DATES_FILE = ".loaded.pickle"
+
+last_loaded_date = {}
+if os.path.exists(LOADED_DATES_FILE):
+    with open(LOADED_DATES_FILE, "rb") as f:
+        last_loaded_date = pickle.load(f)
 
 
 def create_parser():
@@ -12,13 +26,41 @@ def create_parser():
     return parser
 
 
-def put_into_db(text, date, url):
-    print url, ":", date
+# db = psycopg2.connect(database='raw_news', user='crawler')
+# inserter = db.cursor()
+# def put_into_db(text, date, source_url, article_url):
+    # inserter.execute("INSERT INTO news (text, publish_date, source_url, article_url) VALUES (%s, %s, %s, %s)",
+    #                  (text, date, source_url, article_url))
+
+
+def put_into_db(text, date, site_url, article_url):
+    print site_url, ":", article_url, ":", date
 
 
 loaders = {
-    "rss": rss.load
+    "rss": rss_feed.load,
+    "vk":  vk_feed.load
 }
+
+
+def load(link):
+    typ = link["type"]
+    link_url = link["url"]
+    count = 0
+
+    max_date = last_loaded_date[link_url] if link_url in last_loaded_date else datetime.fromtimestamp(0)
+    for (text, date, url) in loaders[typ](link_url):
+        if link_url in last_loaded_date and last_loaded_date[link_url] >= date:
+            break
+
+        max_date = max(max_date, date)
+        put_into_db(text, date, link_url, url)
+        count += 1
+        if count == MAX_FEED_COUNT:
+            break
+
+    last_loaded_date[link_url] = max_date
+    print "loaded {} feeds for {}".format(count, link_url)
 
 
 def main():
@@ -32,9 +74,10 @@ def main():
 
     while True:
         for link in config["links"]:
-            t = link["type"]
-            for (text, data, url) in loaders[t](link["url"]):
-                put_into_db(text, data, url)
+            load(link)
+
+        with open(LOADED_DATES_FILE, "wb") as f:
+            pickle.dump(last_loaded_date, f)
 
         time.sleep(config["period"])
 
