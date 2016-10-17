dc824c83c28ccba97895fc5d2f58968564dd5885
roenke <roenke54@gmail.com>
2016-10-17 23:47:27 +0300

    Add sql for creating of database tables

---
+++ data-server/sql/create_tables.sql
@@ -0,0 +1,19 @@
+create table raw_news (
+	id               	serial primary key,
+    content_of_news  	text,
+	publish_date 		timestamp,
+	source_url 			text,
+	article_url	 		text
+);
+
+create table geo_news (
+  	id 					serial primary key,
+    raw_news_id			int references raw_news (id),
+    coord				point
+);
+
+create table news (
+    id 					serial primary key,
+    geo_news_id			int references geo_news (id),
+    category			int
+);
