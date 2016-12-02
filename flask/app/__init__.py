d8bbb4fc7d4a7ed57ba05ae4ca82f7ac5edf6484
svloyso <svloyso@gmail.com>
2016-12-02 03:06:59 +0300

    Rectangles and dates

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask_googlemaps import GoogleMaps
+from flask_bootstrap import Bootstrap
 
 
 app = Flask(__name__, template_folder='templates')
@@ -8,5 +9,6 @@ app.config.from_object('config')
 db = SQLAlchemy(app)
 
 GoogleMaps(app)
+Bootstrap(app)
 
 from app import views, models
--- flask/app/views.py
+++ flask/app/views.py
@@ -1,22 +1,45 @@
-from flask import render_template
+from flask import render_template, session, request
 from flask_googlemaps import Map
 from app import app
-from .forms import SearchForm
 import urllib.request
 import json
+from flask.ext.wtf import Form
+from wtforms import TextField, SubmitField, DateField, validators
+from flask.ext.admin.form.widgets import DatePickerWidget
 
+class SearchForm(Form):
+    data = TextField('search')
+    search = SubmitField(label='search')
+    date_from = TextField('From')
+    date_to = TextField('To')
 
-def search_events(string):
+def search_events(query_data):
+    print("here8")
     url = app.config['ELASTIC_URL']
     content_maxlen = 150
     query_size = 10000
-    string = string if string else "*"
-    query = {
-        "query": {
-            "wildcard": {"content": string.lower()}
-        },
+    query = { "query": { "bool": {
+            "must": [{
+                "wildcard": {"content": query_data['query'].lower()}
+            }, {
+                "range": {"publish_date": {
+                    "gte": query_data['from'],
+                    "lt": query_data['to']
+                    }}
+            }]}},
+            "filter": {"geo_bounding_box": { "location": {
+                "top_left": {
+                    "lat": query_data['bounds']['tl']['lat'],
+                    "lon": query_data['bounds']['tl']['lng']
+                },
+                "bottom_right": {
+                    "lat": query_data['bounds']['rb']['lat'],
+                    "lon": query_data['bounds']['rb']['lng']
+                }
+            }}},
         "size": query_size
     }
+    print("Query is: ", query)
     req = urllib.request.Request(url, json.dumps(query).encode('utf8'))
     resp = urllib.request.urlopen(req, json.dumps(query).encode())
     markers = [{
@@ -37,19 +60,57 @@ def search_events(string):
     return res
 
 
+startlat = 60.0092659
+startlng = 30.353306
+
 @app.route("/", methods=['GET', 'POST'])
 @app.route("/index", methods=['GET', 'POST'])
 def mapview():
-    # creating a map in the view
+    if 'searchRect' not in session:
+        session['searchRect'] = {
+            'north': startlat + 0.1,
+            'south': startlat - 0.1,
+            'east' : startlng + 0.1,
+            'west' : startlng - 0.1
+        }
     form = SearchForm()
     markers = []
+    query_data = {
+        "query": "*",
+        "from": "now-1000d/d",
+        "to": "now/d",
+        "bounds": {
+            "tl": {
+                "lat": startlat + 0.5,
+                "lng": startlng - 0.5
+            },
+            "rb": {
+                "lat": startlat - 0.5,
+                "lng": startlng + 0.5
+            }
+        }
+    }
     if form.validate_on_submit():
-        markers = search_events(form.search.data)
-    """markers = [{
-        'lat': e.lat,
-        'lng': e.lng,
-        'infobox': "<h3>{}</h3><hr>{}".format(e.title, e.description)
-    } for e in models.Events.query.all()]"""
+        print("here1")
+        if 'rectCheckbox' in request.form and request.form['rectCheckbox'] == "on":
+            print("here2")
+            r = session['searchRect'] = json.loads(request.form['searchRect'])
+            query_data['bounds']['tl']['lat'] = r['north']
+            query_data['bounds']['tl']['lng'] = r['west']
+            query_data['bounds']['rb']['lat'] = r['south']
+            query_data['bounds']['rb']['lng'] = r['east']
+        print("here3")
+        if form.date_from.data and form.date_to.data:
+            print("here4")
+            query_data['from'] = form.date_from.data
+            query_data['to'] = form.date_to.data
+        print("here5")
+        if form.data.data:
+            print("here6")
+            query_data['query'] = form.data.data
+        print("here7")
+        markers = search_events(query_data)
+
     mymap = Map(
         identifier="mymap",
         style=(
@@ -59,10 +120,10 @@ def mapview():
             "left:0;"
             "position:absolute;"
         ),
+        lat=startlat,
+        lng=startlng,
         cluster=True,
-        lat=60.0092659,
-        lng=30.353306,
         zoom=10,
-        markers=markers
+        markers=markers,
     )
-    return render_template('index.html', mymap=mymap, form=form)
+    return render_template('index.html', mymap=mymap, form=form, searchRect=session['searchRect'])
